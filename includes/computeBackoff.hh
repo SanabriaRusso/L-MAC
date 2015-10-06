52d5591b6964fb51879ae1c51691419af7d9bfba
Luis Sanabria-Russo <sanabriarusso@gmail.com>
2015-10-06 20:20:57 +0200

    Fixed an issue with the backoff and the gamma in L-ZC

--- includes/computeBackoff.hh
+++ includes/computeBackoff.hh
@@ -2,8 +2,8 @@
 
 using namespace std;
 
-void computeBackoff(int backoffStage, const int MAXSTAGE, double &counter, int backlog, int ack, std::bitset<CYCLE> slots,
-	double gamma){
+void computeBackoff(int backoffStage, const int MAXSTAGE, double &counter, 
+	int backlog, int ack, std::bitset<CYCLE> slots,	double gamma){
 
 	int CWmin = CYCLE;
 	int txDebug = 0;
@@ -15,13 +15,29 @@ void computeBackoff(int backoffStage, const int MAXSTAGE, double &counter, int b
 			if(txDebug == 1) cout << "Success: " << counter << endl;
 			return;
 		}
-
-		int coin = rand() % (int)(100 - 1) + 1;
-		if(txDebug == 1) cout << "Coin: " << coin << endl;
-		if(coin < (int) (gamma * 100)){
-			counter = CWmin -1;
-			if(txDebug == 1) cout << "Reusing the slot after collision" << coin << endl;
-			return;
+		
+		if(gamma < 1){
+			int coin = rand() % (int)(100 - 1) + 1;
+			if(txDebug == 1) cout << "Coin: " << coin << endl;
+			if(coin < (int) (gamma * 100)){
+				counter = CWmin -1;
+				if(txDebug == 1) cout << "Reusing the slot after collision " << coin << endl;
+				return;
+			}else{
+				if(backoffStage == 0){
+					do{
+					// if(counter != 0) cout << "Reusing counter: " << counter << " becasue (" << slots.test(counter) << ")" << endl;
+					counter = rand() % (int)  (CWmin -1) + 1;		
+					}while( (counter == 0) && (slots.test(counter)) );
+					if(txDebug == 1) cout << "Not reusing, random " << counter << endl;
+				}else{
+					do{
+					// if(counter != 0) cout << "Reusing counter: " << counter << " becasue (" << slots.test(counter) << ")" << endl;
+					counter = rand() % (int)  (pow(2,backoffStage) * CWmin -1) + 1;		
+					}while(counter == 0);
+					if(txDebug == 1) cout << "Not reusing, random " << counter << endl;				
+				}
+			}
 		}else{
 			if(backoffStage == 0){
 				do{
@@ -32,11 +48,32 @@ void computeBackoff(int backoffStage, const int MAXSTAGE, double &counter, int b
 			}else{
 				do{
 					// if(counter != 0) cout << "Reusing counter: " << counter << " becasue (" << slots.test(counter) << ")" << endl;
-					counter = rand() % (int)  (pow(2,backoffStage) -1) + 1;		
+					counter = rand() % (int)  (pow(2,backoffStage) * CWmin -1) + 1;		
 				}while(counter == 0);
-				if(txDebug == 1) cout << "Not reusing, random " << counter << endl;				
+				if(txDebug == 1) cout << "Not reusing, random " << counter << endl;
 			}
-		}		
+		}
+
+
+		// if(coin < (int) (gamma * 100)){
+		// 	counter = CWmin -1;
+		// 	if(txDebug == 1) cout << "Reusing the slot after collision " << coin << endl;
+		// 	return;
+		// }else{
+		// 	if(backoffStage == 0){
+		// 		do{
+		// 			// if(counter != 0) cout << "Reusing counter: " << counter << " becasue (" << slots.test(counter) << ")" << endl;
+		// 			counter = rand() % (int)  (CWmin -1) + 1;		
+		// 		}while( (counter == 0) && (slots.test(counter)) );
+		// 		if(txDebug == 1) cout << "Not reusing, random " << counter << endl;
+		// 	}else{
+		// 		do{
+		// 			// if(counter != 0) cout << "Reusing counter: " << counter << " becasue (" << slots.test(counter) << ")" << endl;
+		// 			counter = rand() % (int)  (pow(2,backoffStage) * CWmin -1) + 1;		
+		// 		}while(counter == 0);
+		// 		if(txDebug == 1) cout << "Not reusing, random " << counter << endl;				
+		// 	}
+		// }		
 	}
 
 }
--- includes/pickNewPacket.hh
+++ includes/pickNewPacket.hh
@@ -3,7 +3,9 @@ using namespace std;
 
 void pickNewPacket(Packet &packet, FIFO <Packet> &Q, int id, double pickupTime)
 {
-	packet = Q.GetFirstPacket();
+	int alwaysSaturated = 1;
+
+	if(alwaysSaturated == 0) packet = Q.GetFirstPacket();
 	packet.contention_time = pickupTime;
 
 }
